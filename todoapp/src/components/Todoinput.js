bcf40c0490d18262a08775f22ef742cb6f4b40ed
shreyasbangera <shreyasybangera1@gmail.com>
2023-02-04 18:29:52 +0530

    updated css

--- todoapp/src/components/Todoinput.js
+++ todoapp/src/components/Todoinput.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Todoform = ({handleSubmit, todo, setTodo, editId}) => {
-
+const Todoform = ({ handleSubmit, todo, setTodo, editId }) => {
   return (
-    <form className='todoinput' onSubmit={handleSubmit}>
-      <input type = "text" value={todo} placeholder='Enter your Todo' onChange={(e)=> setTodo(e.target.value)} />
-      <button type ="submit" >{editId ? "Edit":"Add"}</button>
+    <form className="todoinput" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={todo}
+        placeholder="Enter your Todo"
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit">{editId ? "Edit" : "Add"}</button>
     </form>
-  )
-}
+  );
+};
 
-export default Todoform
+export default Todoform;
--- todoapp/src/components/Todolist.js
+++ todoapp/src/components/Todolist.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import {MdEdit, MdDelete} from 'react-icons/md'
+import React from "react";
+import { MdEdit, MdDelete } from "react-icons/md";
 
-const Todolist = ({todos,handleDelete, handleEdit}) => {
+const Todolist = ({ todos, handleDelete, handleEdit }) => {
   const handleChecked = (e) => {
     e.target.classList.toggle("checked");
   };
 
   return (
     <ul className="todolist">
-    {todos && todos.map(({todo, id})=>(
-    <li className='singletodo'>
-    <span className='todotext' key={id} onClick={handleChecked}>{todo}</span>
-    <button className='listbtn'onClick={() => handleEdit(id)}><MdEdit /></button>
-    <button className='listbtn' onClick={()=>handleDelete(id)}><MdDelete /></button>
-    </li>
-    ))}
+      {todos &&
+        todos.map(({ todo, id }) => (
+          <li className="singletodo">
+            <span className="todotext" key={id} onClick={handleChecked}>
+              {todo}
+            </span>
+            <button className="listbtn" onClick={() => handleEdit(id)}>
+              <MdEdit />
+            </button>
+            <button className="listbtn" onClick={() => handleDelete(id)}>
+              <MdDelete />
+            </button>
+          </li>
+        ))}
     </ul>
+  );
+};
 
-    
-  )
-}
-
-export default Todolist
+export default Todolist;
