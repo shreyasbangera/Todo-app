0b4e0b2e0436e41aef2ca4d5902037e7c8ce891b
shreyasbangera <shreyasybangera1@gmail.com>
2023-02-04 18:11:50 +0530

    updated css

--- todoapp/src/components/Todoinput.js
+++ todoapp/src/components/Todoinput.js
@@ -3,12 +3,10 @@ import React from 'react'
 const Todoform = ({handleSubmit, todo, setTodo, editId}) => {
 
   return (
-    <div>
     <form className='todoinput' onSubmit={handleSubmit}>
       <input type = "text" value={todo} placeholder='Enter your Todo' onChange={(e)=> setTodo(e.target.value)} />
       <button type ="submit" >{editId ? "Edit":"Add"}</button>
     </form>
-    </div>
   )
 }
 
--- todoapp/src/components/Todolist.js
+++ todoapp/src/components/Todolist.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import {MdEdit, MdDelete} from 'react-icons/md'
 
 const Todolist = ({todos,handleDelete, handleEdit}) => {
+  const handleChecked = (e) => {
+    e.target.classList.toggle("checked");
+  };
+
   return (
-    
     <ul className="todolist">
     {todos && todos.map(({todo, id})=>(
     <li className='singletodo'>
-    <span className='todotext' key={id}>{todo}</span>
-    <button className='listbtn'onClick={() => handleEdit(id)}>Edit</button>
-    <button className='listbtn' onClick={()=>handleDelete(id)}>Delete</button>
+    <span className='todotext' key={id} onClick={handleChecked}>{todo}</span>
+    <button className='listbtn'onClick={() => handleEdit(id)}><MdEdit /></button>
+    <button className='listbtn' onClick={()=>handleDelete(id)}><MdDelete /></button>
     </li>
     ))}
     </ul>
